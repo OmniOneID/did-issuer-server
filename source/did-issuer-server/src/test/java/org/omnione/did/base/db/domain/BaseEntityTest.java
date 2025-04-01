f4dcb9e0271a5c7807b247643490eeda023ff0bd
gwnam <gwnam@raoncorp.com>
2025-04-01 13:37:34 +0900

    chore: Clean up import of deleted class

--- source/did-issuer-server/src/test/java/org/omnione/did/base/db/domain/BaseEntityTest.java
+++ source/did-issuer-server/src/test/java/org/omnione/did/base/db/domain/BaseEntityTest.java
@@ -4,7 +4,6 @@ import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.omnione.did.base.config.JpaConfig;
 import org.omnione.did.base.config.QuerydslConfig;
-import org.omnione.did.issuer.TestStubConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
